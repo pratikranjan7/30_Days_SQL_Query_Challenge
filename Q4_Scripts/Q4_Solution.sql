ba2b79c334917477aba9a06df5ae78be7c6020cb
Pratik Ranjan <pratikranjan4597@gmail.com>
2024-04-09 18:20:31 +0530

    Added SQL script of Day-4

---
+++ Q4_Scripts/Q4_Solution.sql
@@ -0,0 +1,26 @@
+create table Q4_data
+(
+    id int,
+    name varchar(20),
+    location varchar(20)
+);
+insert into Q4_data
+values(1, null, null);
+insert into Q4_data
+values(2, 'David', null);
+insert into Q4_data
+values(3, null, 'London');
+insert into Q4_data
+values(4, null, null);
+insert into Q4_data
+values(5, 'David', null);
+
+select *
+from Q4_data;
+
+
+select min(id) as id, min(name) as name, min(location) as location
+from Q4_data
+
+select max(id) as id, min(name) as name, min(location) as location
+from Q4_data
