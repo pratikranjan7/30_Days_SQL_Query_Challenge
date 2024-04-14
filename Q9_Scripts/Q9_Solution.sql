67fb1ceb73d6bf56b0a098e7ece69e976f7ba042
Pratik Ranjan <pratikranjan4597@gmail.com>
2024-04-14 21:58:52 +0530

    Q9_Solution

---
+++ Q9_Scripts/Q9_Solution.sql
@@ -0,0 +1,9 @@
+-- Solution:
+
+    select dates, cast(product_id as varchar) as products
+    from orders
+union
+    select dates, string_agg(cast(product_id as varchar),',') as products
+    from orders
+    group by customer_id, dates
+order by dates, products;
